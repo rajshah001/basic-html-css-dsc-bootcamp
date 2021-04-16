dbf31d6139d91f1aac42fa6a0ce879f748cc12eb
rajshah001 <rajshah30601@gmail.com>
2021-04-16 23:27:59 +0530

    basic HTML and CSS code snippets

---
+++ script.js
@@ -0,0 +1,8 @@
+function file_hello() {
+    console.log("Hello JavaScript from js file");
+}
+
+function showMessage(){
+    var message = document.getElementById("name").value;
+    form_value.innerHTML= "Hello " + message + " !!";
+}
